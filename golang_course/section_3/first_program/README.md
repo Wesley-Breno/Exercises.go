971b1c7d1b5e511d926ad4df017a944daa0ff285
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2024-07-03 16:18:52 -0300

    My first program on Go

---
+++ golang_course/section_3/first_program/README.md
@@ -0,0 +1,2 @@
+This is my first Go program where I write two messages on the screen
+and I leave comments in the code informing conventions of the Go language.s
---
+++ golang_course/section_3/first_program/first_program.go
@@ -0,0 +1,14 @@
+// Executable programs start with the 'main' package
+package main
+
+/*
+Go codes are organized into packages
+and to use them it is necessary to declare one or several imports
+*/
+import "fmt"
+
+// The gateway to a Go program is the 'main()' function
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
